c9f2b91f08e247b55fd7645d1b4c335273e06bae
ki43 <k22103kk@aitech.ac.jp>
2024-02-22 01:03:34 +0900

    rockの挙動の修正
    岩がつっかからないようにはしました

--- Assets/script/gimmick/rock/rock_move.cs
+++ Assets/script/gimmick/rock/rock_move.cs
@@ -6,11 +6,20 @@ public class rock_move : MonoBehaviour
 {
     Rigidbody rb;
     public Vector3 normal_fall = new Vector3(-2f, -3.0f, 0);
+
     public Vector3 rev_time_fall = new Vector3(2f, 3.0f, 0);
+
     public Vector3 normal_slope = new Vector3(-1.0f, -1.8f, 0);
+
     public Vector3 rev_time_slope =  new Vector3(3.0f, -1.8f, 0);
+
     public Vector3 normal_cliff = new Vector3(-2f, -2.0f, 0);
+
     public Vector3 rev_time_cliff = new Vector3(3f, 2.0f, 0);
+
+    public Vector3 rev_time_flat = new Vector3(1f, 0, 0);
+
+    public Vector3 normal_flat = new Vector3(-1f, 0, 0);
 
    
     // Start is called before the first frame update
@@ -48,17 +57,11 @@ public class rock_move : MonoBehaviour
             //Debug.Log("vertical");
             if (TIME_MANAGER.is_revtime == true)
             {
-                //rb.velocity = new Vector3(0, 3.0f, 0);
                 this.gameObject.GetComponent<Rigidbody>().velocity = rev_time_fall;
-                
             }
             else if (TIME_MANAGER.is_revtime == false)
             {
                 this.gameObject.GetComponent<Rigidbody>().velocity = normal_fall;
-
-                //Debug.Log($"vertical normal time {rb.velocity}");
-
-
             }
 
         }
@@ -73,6 +76,19 @@ public class rock_move : MonoBehaviour
             else
             {
                 rb.velocity = rev_time_cliff;
+            }
+        }
+
+        if (other.CompareTag("flat"))
+        {
+            Debug.Log("ok");
+            if (TIME_MANAGER.is_revtime == false)
+            {
+                rb.velocity = normal_flat;
+            }
+            else
+            {
+                rb.velocity = rev_time_flat;
             }
         }
 
