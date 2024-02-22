73a3b9f08201e24910fb7ca419cb918a7a4dcfaf
ki43 <k22103kk@aitech.ac.jp>
2024-02-22 17:00:59 +0900

    一メモリごとに減る仕様に変更
    詳しい仕様はイシュー文に合わせたつもりです

--- Assets/script/player/TIME_MANAGER.cs
+++ Assets/script/player/TIME_MANAGER.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+
 //このクラスの is_revtime 変数は 頻繁に他のクラスから参照されます　ki43IIIより
 public class TIME_MANAGER : MonoBehaviour
 {
@@ -12,15 +14,18 @@ public class TIME_MANAGER : MonoBehaviour
     public Slider debug_slider;
 
     public int max_clock = 20;
+    float current_clock_value;
 
     public bool Force_change = false;
     public bool decreasing_gage = true;
 
+    
     // Start is called before the first frame update
     void Start()
     {
-        debug_slider.maxValue = max_clock;
-        debug_slider.value = max_clock - Time.time;
+        debug_slider.maxValue = 8;
+        debug_slider.value = 8;
+        current_clock_value = max_clock - Time.time;
     }
 
     // Update is called once per frame
@@ -44,7 +49,46 @@ public class TIME_MANAGER : MonoBehaviour
         {
             if (decreasing_gage == true)
             {
-                debug_slider.value -= Time.deltaTime;
+                current_clock_value -= Time.deltaTime;
+
+                //そのうち短縮版のコードを書きます
+                if ((max_clock / 8) * 8 < current_clock_value)
+                {
+                    debug_slider.value = 8;
+                }
+                else if ((max_clock / 8) * 7 < current_clock_value)
+                {
+                    debug_slider.value = 7;
+                }
+                else if ((max_clock / 8) * 6 < current_clock_value)
+                {
+                    debug_slider.value = 6;
+                }
+                else if ((max_clock / 8) * 5 < current_clock_value)
+                {
+                    debug_slider.value = 5;
+                }
+                else if ((max_clock / 8) * 4 < current_clock_value)
+                {
+                    debug_slider.value = 4;
+                }
+                else if ((max_clock / 8) * 3 < current_clock_value)
+                {
+                    debug_slider.value = 3;
+                }
+                else if ((max_clock / 8) * 2 < current_clock_value)
+                {
+                    debug_slider.value = 2;
+                }
+                else if ((max_clock / 8) * 1 < current_clock_value)
+                {
+                    debug_slider.value = 1;
+                }
+                else
+                {
+                    debug_slider.value = 0;
+                }
+
             }
             
             if (debug_slider.value <= 0 && Force_change == true)
@@ -57,4 +101,3 @@ public class TIME_MANAGER : MonoBehaviour
 
     }
 }
-
