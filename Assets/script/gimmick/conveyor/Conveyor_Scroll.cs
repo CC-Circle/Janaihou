719cccf32ca2989b4bff580927b129c465781e7f
ki43 <k22103kk@aitech.ac.jp>
2024-02-26 22:01:06 +0900

    ベルトコンベアーの流れの見た目の修正
    見た目の部分だけ触りました
    他は変わっていないのでご心配なく

--- Assets/script/gimmick/conveyor/Conveyor_Scroll.cs
+++ Assets/script/gimmick/conveyor/Conveyor_Scroll.cs
@@ -7,12 +7,16 @@ public class Conveyor_Scroll : MonoBehaviour
     private Renderer m_renderer = null;
     [SerializeField] private float m_speed = 1.0f;
 
+
     private Vector2 m_offset = Vector2.zero;
+    Material[] mats;//今回マテリアルが複数個あったので
+
 
     // Start is called before the first frame update
     void Start()
     {
         m_renderer = GetComponent<Renderer>();
+        mats = m_renderer.materials;
     }
 
     // Update is called once per frame
@@ -21,14 +25,14 @@ public class Conveyor_Scroll : MonoBehaviour
         // オブジェクトの forward 方向に合わせて移動量を計算
         Vector3 forwardDirection = transform.forward;
         if (TIME_MANAGER.is_revtime)
-        {
-            m_offset += new Vector2(forwardDirection.x, forwardDirection.z) * m_speed * Time.deltaTime;
+        {   
+            m_offset += Vector2.up * m_speed * Time.deltaTime;
         }
         else
         {
-            m_offset -= new Vector2(forwardDirection.x, forwardDirection.z) * m_speed * Time.deltaTime;
+            m_offset -= Vector2.up * m_speed * Time.deltaTime;
         }
         // マテリアルに offset をセット
-        m_renderer.material.mainTextureOffset = m_offset;
+        mats[0].mainTextureOffset = m_offset;
     }
 }
