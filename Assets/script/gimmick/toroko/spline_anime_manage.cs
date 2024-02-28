5c6304d51a2d2e8425ff172b9e4d09587e16643b
ki43 <k22103kk@aitech.ac.jp>
2024-02-29 01:30:19 +0900

    必要なデータの読み込み

---
+++ Assets/script/gimmick/toroko/spline_anime_manage.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+using System;
+
+public class spline_anime_manage : MonoBehaviour
+{
+    [SerializeField] SplineAnimate splineAnimate;
+
+
+    float duration = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = splineAnimate.Duration / 12;
+        StartCoroutine(DelayCoroutine(0.01f, () =>
+        {
+            splineAnimate.ElapsedTime += duration;
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (splineAnimate.Duration > splineAnimate.ElapsedTime + duration)
+            
+            splineAnimate.ElapsedTime += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            if (splineAnimate.ElapsedTime > duration)
+            splineAnimate.ElapsedTime -= Time.deltaTime;
+        }
+
+    }
+
+    //遅延子ルーチン用
+    private IEnumerator DelayCoroutine(float seconds, Action action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action?.Invoke();
+    }
+
+
+}
