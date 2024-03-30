84b67a79ae797a01d903d545b580442f278f077e
ki43 <k22103kk@aitech.ac.jp>
2024-03-30 16:01:17 +0900

    ui仮配置

---
+++ Assets/UI_Base.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Base : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI debug_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debug_text.text = $"debug message\n" +
+            $"screen size w{Screen.width} x h{Screen.height}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
