b03eff2e454e5b4d37531903a6845b7cfa32825c
ki43 <k22103kk@aitech.ac.jp>
2024-04-02 00:24:07 +0900

    各シーンへの移動を追加

--- Assets/script/UI/Button_manage_ui.cs
+++ Assets/script/UI/Button_manage_ui.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Button_manage_ui : MonoBehaviour
 {
@@ -11,7 +12,9 @@ public class Button_manage_ui : MonoBehaviour
     public TextMeshProUGUI stage_indication;
     public List<Sprite> stage_preview_list;
     public Image stage_preview;
-    
+
+    string buttonName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +34,7 @@ public class Button_manage_ui : MonoBehaviour
     public void touch_stage_select()
     {
         GameObject clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
-        string buttonName = clickedButton.name;
+        buttonName = clickedButton.name;
         //Debug.Log("Clicked button name: " + buttonName);
         current_pointer_obj.transform.position = clickedButton.transform.position;
         stage_indication.text = $"STAGE\n{buttonName}";
@@ -42,6 +45,35 @@ public class Button_manage_ui : MonoBehaviour
         else
         {
             stage_preview.sprite = stage_preview_list[int.Parse(buttonName) - 1];
+        }
+    }
+
+    public void touch_start_button()
+    {
+        
+        switch (buttonName)
+        {
+            case "1":
+                SceneManager.LoadScene("Takumi");
+                break;
+            case "2":
+                SceneManager.LoadScene("Takumi 1");
+                break;
+            case "3":
+                SceneManager.LoadScene("Takumi 2");
+                break;
+            case "4":
+                SceneManager.LoadScene("Takumi 3");
+                break;
+            case "5":
+                SceneManager.LoadScene("Takumi4");
+                break;
+            case "TUTORIAL":
+                SceneManager.LoadScene("Tutorial");
+                break;
+
+            default:
+                break;
         }
     }
 }
